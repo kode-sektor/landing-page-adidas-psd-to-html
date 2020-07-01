564fd85458933d3f60202b1ff18a883ee0f27955
Olagoke Kay <60120856+Kardinalkay@users.noreply.github.com>
2020-07-01 19:35:44 -0400

    Implement product-colour-change functionality

--- js/index.js
+++ js/index.js
@@ -1,8 +1,33 @@
 // Navigation-toggle functionality
 
-let $navToggle = document.getElementById('nav-toggle');
+const $navToggle = document.getElementById('nav-toggle');
 const $nav = document.getElementById('main-nav-links');
 
+// On toggle of nav menu, hide / show navigation
 $navToggle.addEventListener('click', () => {
 	$nav.classList.toggle('show');
 });
+
+
+// Product-colour change functionality
+
+const $productColour = document.getElementById('product-colour');	// Reference to text displaying colour name
+
+const $btnColours = document.querySelectorAll('.product-btn-set > button');	// reference to colour buttons clicked
+let $productImg = document.querySelector('.showglass img');	// Reference to showglass image
+
+for (let i=0; i < $btnColours.length; i++) {	// If clicked, cycle and fetch id of clicked button
+	let $btn = $btnColours[i];
+	$btn.addEventListener('click', (e) => {
+		let colour = (e.target).id;	// Get the id (colour title)
+		let newImgSrc = "img/adidas-shoe-" + colour + ".png";	// Form URL
+		colour = colour.toUpperCase();	// Transform
+		$productImg.src = newImgSrc;	// Set the right image URL
+		$productColour.innerText = `(${colour})`;	// Set the right colour heading
+	});
+}
+
+
+
+
+
